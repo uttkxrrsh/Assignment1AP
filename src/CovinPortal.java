2a5e30aab3b0a2bf4d89a88406e54259ec5e9634
uttkxrrsh <uttkarsh20479@iiitd.ac.in>
2021-10-10 04:14:57 +0530

    made datastructs

--- src/CovinPortal.java
+++ src/CovinPortal.java
@@ -1,8 +1,34 @@
+import java.util.*;
 
 public class CovinPortal {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static int uhidgen=100000;
+	
+	public static ArrayList<Hospital> hList = new ArrayList<>();
+	
+	public static ArrayList<Citizen> cList = new ArrayList<>();
+	
+	public static ArrayList<Slot> slots = new ArrayList<>();
+	
+	public static ArrayList<Vaccine> vaccine = new ArrayList<>();
+	
+	public static void regHospital() {
+		String hname;
+		String pincode;
+		System.out.println("Hospital Name: ");
+		hname = sc.nextLine();
+		System.out.println("PinCode: ");
+		pincode = sc.nextLine();
+		Hospital hospital = new Hospital(hname, pincode,uhidgen);
+		hList.add(hospital);
+		uhidgen++;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
